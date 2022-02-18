45118a6a601e6abc9cb8287b5739b80234c09cc3
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-19 00:57:38 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -20,13 +20,12 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-    int count = 0;
-    int prime = 0;
+    int prime = 0, count = 0;
     while (true) {
         prime++;
         if (checkPrime(prime))
-            count++;
-        if (n == count)
+        count++;
+        if (count == n)
             return prime;
     }
     return count;
@@ -42,14 +41,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    int prime = 2;
-    while (prime < hbound) {
-        if (checkPrime(prime)) {
+    int prime, sum = 0;
+    uint64_t prime, sum = 0;
+    for (prime = 0; prime < hbound; prime++) {
+        if (checkPrime(prime))
             sum += prime;
-        }
-        prime++;
     }
     return sum;
-}
 }

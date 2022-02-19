4f777ac3c8495b96f656df3ba42e8dc69a25d3a8
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-19 19:18:11 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -1,44 +1,45 @@
 #include <cstdint>
 #include <cmath>
 #include "alg.h"
+
+
 bool checkPrime(uint64_t value) {
-  if (value < 2) {
-    return false;
-  }
-  if (value == 2) {
-    return true;
-  }
-  for (int prime = 2; prime <= sqrt(value); prime++) {
-    if (value % prime == 0) {
-      return false;
+    int prime;
+    if (value < 2)
+            return false;
+    for (prime = 2; prime <= value; prime++) {
+        if (value%prime == 0)
+            if (value != prime)
+                return false;
     }
-  }
-  return true;
+    return true;
 }
 uint64_t nPrime(uint64_t n) {
-  uint64_t num = 0;
-  for (int prime = 2; ; prime++) {
-    if (checkPrime(prime)) {
-       num += 1;
-       if (num == n) {
-         return prime;
-       }
-     }
-  }
+    int prime = 0, count = 0;
+    while (true) {
+        prime++;
+        if (checkPrime(prime))
+            count++;
+        if (count == n)
+            return prime;
+    }
+    return count;
 }
 uint64_t nextPrime(uint64_t value) {
-  for (int prime = value + 1; ; prime++) {
-    if (checkPrime(prime)) {
-      return prime;
+    int a = value+1;
+    while (true) {
+        if (checkPrime(a))
+            return a;
+        a++;
     }
-  }
+    return a;
 }
+
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (int prime = 0; prime < hbound; prime++) {
-    if (checkPrime(prime)) {
-      sum += prime;
-    }
-  }
-  return sum;
+    int sum = 0, prime;
+    uint64_t sum = 0, prime;
+    for (prime = 0; prime < hbound; prime++)
+    if (checkPrime(prime))
+        sum += prime;
+    return sum;
 }

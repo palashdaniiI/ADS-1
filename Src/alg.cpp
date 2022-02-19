6c18081977a581ee8ece50af1ff9f44995819ba8
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-19 16:26:28 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -3,37 +3,35 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-   bool isPrime = true;
-   if (value == 0 || value == 1)
-      return false;
-   for (int i = 2; i <= value / 2; ++i) { 
-       if (value % i == 0)
-           if (value != i)
-               return false; 
-   }
-   return true;
+bool isPrime = true;
+if (value == 0 || value == 1)
+return false;
+for (int i = 2; i <= value / 2; ++i) { 
+if (value % i == 0)
+if (value != i)
+return false; 
 }
-
+return true;
+}
 uint64_t nPrime(uint64_t n) {
-    int prime = 0, count = 0;
-    while (true) {
-        prime++;
-        if (checkPrime(prime))
-            count++;
-        if (count == n)
-            return prime;
-    }
-    return count;
+int prime = 0, count = 0;
+while (true) {
+prime++;
+if (checkPrime(prime))
+count++;
+if (count == n)
+return prime;
 }
-
+return count;
+}
 uint64_t nextPrime(uint64_t value) {
-    int prime = value+1;
-    while (true) {
-         if (checkPrime(prime))
-             return prime;
-         prime++;
-    }
-    return prime;
+int prime = value+1;
+while (true) {
+if (checkPrime(prime))
+return prime;
+prime++;
+}
+return prime;
 }
 
 uint64_t sumPrime(uint64_t hbound) {

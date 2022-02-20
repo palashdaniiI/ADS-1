123f08b6e0d44a0f3b47c3e2ae4490f80d8fbb46
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-20 14:09:14 +0300

    Update alg.cpp

--- Src/alg.cpp
+++ Src/alg.cpp
@@ -1,45 +1,54 @@
-#include <cstdint>
 #include <cmath>
+#include <climits>
 #include "alg.h"
-
+#include <cstdint>
 
 bool checkPrime(uint64_t value) {
-    int prime;
-    if (value < 2)
-            return false;
-    for (prime = 2; prime <= value; prime++) {
-        if (value%prime == 0)
-            if (value != prime)
-                return false;
+// вставьте код функции
+  bool isPrime = true;
+    uint64_t del = sqrt(value);
+    if ((value % 2 == 0 && value != 2) || value < 2) isPrime = false;
+    for (uint64_t prime = 2 ; prime <= del ; prime++) {
+        if (value % prime == 0) {
+            isPrime = false;
+            break;
+        }
     }
-    return true;
+    return isPrime;
 }
 uint64_t nPrime(uint64_t n) {
-    int prime = 0, count = 0;
-    while (true) {
-        prime++;
-        if (checkPrime(prime))
-            count++;
-        if (count == n)
-            return prime;
+// вставьте код функции
+  uint64_t count = 0;
+    uint64_t prime = 0;
+        for (uint64_t j = 2 ; j <= ULONG_MAX ; j++) {
+            if (checkPrime(j)) {
+                prime += 1;
+                if (prime == n) {
+                    count = j;
+                    break;
+                }
+            }
+        }
+        return count;
+}
+uint64_t nextPrime(uint64_t value) {
+// вставьте код функции
+  uint64_t count = 0;
+    for (uint64_t prime = value + 1 ; prime <= ULONG_MAX ; prime++) {
+        if (checkPrime(prime)) {
+            count = prime;
+            break;
+        }
     }
     return count;
 }
-uint64_t nextPrime(uint64_t value) {
-    int a = value+1;
-    while (true) {
-        if (checkPrime(a))
-            return a;
-        a++;
-    }
-    return a;
-}
-
 uint64_t sumPrime(uint64_t hbound) {
-    int sum = 0, prime;
-    uint64_t sum = 0, prime;
-    for (prime = 0; prime < hbound; prime++)
-    if (checkPrime(prime))
-        sum += prime;
+// вставьте код функции
+  uint64_t sum = 0;
+    for (uint64_t prime = 2 ; prime < hbound ; prime++) {
+        if ( checkPrime(prime) ) {
+            sum += prime;
+        }
+    }
     return sum;
 }

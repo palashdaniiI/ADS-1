42b103393c79890ec623ce2c804616d3c3ef1624
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-02-27 15:27:29 +0300

    Add files via upload

---
+++ src/alg.cpp
@@ -0,0 +1,50 @@
+// Copyright 2022 NNTU-CS
+#include <cstdint>
+#include "alg.h"
+
+
+bool checkPrime(uint64_t value) {
+  bool isPrime = true;
+  uint64_t del = sqrt(value);
+  if ((value % 2 == 0 && value != 2) || value < 2)
+    isPrime = false;
+  for (uint64_t prime = 2 ; prime <= del ; prime++) {
+    if (value % prime == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  return isPrime;
+}
+uint64_t nPrime(uint64_t n) {
+  uint64_t count = 0;
+  uint64_t prime = 0;
+  for (uint64_t j = 2 ; j <= ULONG_MAX ; j++) {
+    if (checkPrime(j)) {
+      prime += 1;
+      if (prime == n) {
+        count = j;
+        break;
+      }
+    }
+  }
+  return count;
+}
+uint64_t nextPrime(uint64_t value) {
+  uint64_t count = 0;
+  for (uint64_t prime = value + 1 ; prime <= ULONG_MAX ; prime++) {
+    if (checkPrime(prime)) {
+      count = prime;
+      break;
+    }
+  }
+}
+uint64_t sumPrime(uint64_t hbound) {
+  uint64_t sum = 0;
+  for (uint64_t prime = 2 ; prime < hbound ; prime++) {
+    if ( checkPrime(prime) ) {
+      sum += prime;
+    }
+  }
+  return sum;
+}
